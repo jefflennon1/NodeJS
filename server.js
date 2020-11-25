b4e04dcec3bf714c32312c7530fec055299dd1b1
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-24 21:12:13 -0300

    conectando com o db

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Iniciando o app
 const app = express();
 
+// Iniciando o Db
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
 
-app.listen('3386',()=>{
+
+app.get('/lista', (request, response)=>{
+  response.json(`qualquer coisa`);
+});
+
+app.listen(3386,()=>{
   console.log('Back-end Started! 🚀');
 })
