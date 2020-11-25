3e65ef236dca91f2ed2a325ee0546a6fe126b196
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-24 21:46:12 -0300

    criando o model de produto

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
 
 // Iniciando o Db
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+ { useUnifiedTopology: true, useNewUrlParser: true });
 
+ requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 app.get('/lista', (request, response)=>{
-  response.json(`qualquer coisa`);
+  Product.create({
+    title: 'React native',
+    description: 'Curso de react native para iniciantes',
+    url: 'www.rocketseat.com.br',
+  });
+
+  return response.json(`qualquer coisa`);
 });
 
 app.listen(3386,()=>{
